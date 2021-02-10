c042e9a892d80da1dc187b855fc1de7d9fc27342
tjjay17 <56407501+tjjay17@users.noreply.github.com>
2021-02-10 05:34:10 -0500

    Refactored Code

--- game.js
+++ game.js
@@ -1,9 +1,7 @@
 
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
 var userClickedPattern = [];
-
 var started = false;
 var level = 0;
 
@@ -16,18 +14,14 @@ $(document).keypress(function() {
 });
 
 $(".btn").click(function() {
-
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-
   playSound(userChosenColour);
   animatePress(userChosenColour);
-
   checkAnswer(userClickedPattern.length-1);
 });
 
 function checkAnswer(currentLevel) {
-
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
       if (userClickedPattern.length === gamePattern.length){
         setTimeout(function () {
@@ -38,11 +32,10 @@ function checkAnswer(currentLevel) {
       playSound("wrong");
       $("body").addClass("game-over");
       $("#level-title").text("Game Over, Press Any Key to Restart");
-
+      
       setTimeout(function () {
         $("body").removeClass("game-over");
       }, 200);
-
       startOver();
     }
 }
